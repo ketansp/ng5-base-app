27b9308130c93626927b57fb1d59e68cb4b67227
Ketan Patil <100.percent.ketan@gmail.com>
2018-04-18 22:47:16 +0530

    Adding components for login and logout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,17 @@ import { HttpRequestInterceptor } from './interceptors/http-request-interceptor'
 import { HttpResponseInterceptor } from './interceptors/http-response-interceptor';
 import { AuthenticationGuard } from './guards/authentication.guard';
 import { RoleGuard } from './guards/role.guard';
+import { AuthenticationComponent } from './views/authentication/authentication.component';
+import { LoginComponent } from './views/authentication/login/login.component';
+import { LogoutComponent } from './views/authentication/logout/logout.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule
